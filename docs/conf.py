7f77b21391f61c73b948a7839910f75ea7ba209a
SC-McD <stewart.charles.mcdowall@protonmail.ch>
2023-08-02 09:43:29 +0200

    update-docs

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ author = 'S.C.McDowall'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autosummary', 'sphinx-autoapi']
+extensions = ['sphinx.ext.autodoc','sphinx.ext.napoleon','sphinx.ext.duration','sphinx.ext.doctest','sphinx.ext.autosummary', 'autoapi.extension']
 
 autoapi_dirs = ['../src/futuram']
 
